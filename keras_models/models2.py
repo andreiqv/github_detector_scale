f5c1cd96fbd0703d6c62eca05c603b2a8db3c818
z <phxv@mail.ru>
2018-11-30 09:35:22 +0300

    1

--- keras_models/models2.py
+++ keras_models/models2.py
@@ -6,10 +6,24 @@ from tensorflow.contrib.slim.nets import resnet_v1, resnet_v2
 from tensorflow.contrib.slim.nets import vgg
 from tensorflow.contrib.slim.nets import alexnet	
 
-def model_resnet(inputs):
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+def model_resnet50(inputs):
 
 	net = resnet_v2.resnet_v2_50
 	x, end_points = net(inputs, num_classes=5, is_training=True)
 	x = layers.Reshape((5,))(x)
-	model = keras.Model(inputs, x, name='glp_model1')
+	model = keras.Model(inputs, x, name='resnet')
 	return model
+
+def model_resnet(inputs):
+
+	base_model = InceptionV3(weights='imagenet', include_top=False, pooling='avg', 
+        input_tensor=inputs)
+	x = base_model.output
+	x = layers.Dense(5, activation='sigmoid')(x)
+	model = keras.Model(inputs=inputs, outputs=x, name='keras_model')	
+	return model
+
+	
