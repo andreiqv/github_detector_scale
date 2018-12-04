24a42fcaf4499a6d17215af93da7608b3010a925
z <phxv@mail.ru>
2018-12-04 19:21:56 +0300

    1

--- keras_models/train_with_slim.py
+++ keras_models/train_with_slim.py
@@ -234,7 +234,7 @@ if __name__ == '__main__':
 					
 					try:
 						features, labels = sess.run(next_element_valid)
-						valid_logits, valid_logits, valid_loss, valid_acc, valid_top6 = sess.run([logits, loss, acc, acc_top6], feed_dict={x: features, y: labels})
+						valid_logits, valid_loss, valid_acc, valid_top6 = sess.run([logits, loss, acc, acc_top6], feed_dict={x: features, y: labels})
 						print('valid:', i, labels[0], valid_logits[0])	
 
 						valid_loss_list.append(np.mean(valid_loss))
