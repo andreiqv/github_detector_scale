20e3609225f8263a23f708343a177289ea63ad22
z <phxv@mail.ru>
2018-12-04 07:21:07 +0300

    with_good_only

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -45,8 +45,8 @@ batch_size = 128  # 256
 #dataset = TfrecordsDataset("../dataset/train-full128x128.tfrecords", "../dataset/test-full128x128.tfrecords", image_shape,
 #                           image_channels, 256)
 
-dataset = TfrecordsDataset("../dataset/train-bboxes128x128.tfrecords", 
-                            "../dataset/test-bboxes128x128.tfrecords", 
+dataset = TfrecordsDataset("../presence_dataset/train-bboxes128x128.tfrecords", 
+                            "../presence_dataset/test-bboxes128x128.tfrecords", 
                             image_shape, image_channels, batch_size)
 
 
