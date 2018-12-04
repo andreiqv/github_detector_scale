7a012cb0c61a9d0d0407d26aea48a2d337d6fa41
z <phxv@mail.ru>
2018-12-04 13:21:56 +0300

    1

---
+++ keras_models/check_dataset.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Прямой проход без хеширования промежуточных данных.
+
+v2 - added saver.
+"""
+	
+# https://github.com/tensorflow/tensorflow/issues/22837#issuecomment-428327601
+
+import tensorflow as tf
+import numpy as np
+import math
+import sys, os
+
+from dataset_factory import GoodsDataset
+#from goods_tf_records import GoodsTfrecordsDataset
+
+# tf.enable_eager_execution()
+import settings
+from settings import IMAGE_SIZE
+from utils.timer import timer
+
+#--
+#------------
+# dataset
+#goods_dataset = GoodsDataset(settings.dataset_list, settings.labels_list, 
+#settings.IMAGE_SIZE, settings.train_batch, settings.valid_batch, settings.multiply, 
+#settings.valid_percentage)
+
+batch_size = 8  # 256
+image_shape = (128, 128)
+image_channels = 3
+dataset = TfrecordsDataset("../dataset/objectness_train-bboxes128x128.tfrecords", 
+	"../dataset/objectness_test-bboxes128x128.tfrecords", 
+	image_shape, image_channels, batch_size)
+
+
+train_dataset = dataset.train_set
+valid_dataset = dataset.test_set.batch(batch_size)
+
+graph = tf.Graph()  # сreate a new graph
+
+with graph.as_default():
+	
+	iterator_train = train_dataset.make_one_shot_iterator()
+	next_element_train = iterator_train.get_next()
+	iterator_valid = valid_dataset.make_one_shot_iterator()
+	next_element_valid = iterator_valid.get_next()
+
+	with tf.Session() as sess:
+		sess.run(tf.global_variables_initializer())
+
+		train_count = 0
+		while True:				
+			try:
+				features, labels = sess.run(next_element_train)
+				train_count += 1
+				if train_count % 20 == 0:
+					print('train_count', train_count)				
+			except tf.errors.OutOfRangeError:
+				print("End of training dataset. Count={} batches".format(train_count))
+				break	
+
+		valid_count = 0
+		while True:				
+			try:
+				features, labels = sess.run(next_element_valid)
+				valid_count += 1	
+				if valid_count % 20 == 0:
+					print('valid_count', valid_count)								
+			except tf.errors.OutOfRangeError:
+				print("End of validation dataset. Count={} batches".format(valid_count))
+				break	
+
+		print('\nTrain={}, valid={}'.format(train_count, valid_count))
