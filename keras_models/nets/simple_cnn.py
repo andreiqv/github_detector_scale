0a2ccc29871b2f6e7d7bbcdcaebe7123f5c820e5
z <phxv@mail.ru>
2018-12-04 14:54:52 +0300

    1

--- keras_models/nets/simple_cnn.py
+++ keras_models/nets/simple_cnn.py
@@ -4,7 +4,7 @@ from settings import IMAGE_SIZE
 
 def cnn(inputs, num_classes=1000, is_training=True):
 
-	x = tf.reshape(inputs, [-1, IMAGE_SIZE[0]*IMAGE_SIZE[1]*3])  # 128 x 128 x 3
+	images = tf.reshape(inputs, [-1, IMAGE_SIZE[0]*IMAGE_SIZE[1]*3])  # 128 x 128 x 3
 	net = slim.conv2d(images, 8, [5,5], scope='conv1')   
 	net = slim.max_pool2d(net, [2,2], scope='pool1')  # 64 x 64  x 8
 	net = slim.conv2d(net, 16, [5,5], scope='conv2')
@@ -17,7 +17,7 @@ def cnn(inputs, num_classes=1000, is_training=True):
 	net = slim.max_pool2d(net, [2,2], scope='pool3')  # 4 x 4 x 32
 	net = slim.flatten(net, scope='flatten3')
 	#net = slim.fully_connected(net, 500, scope='fc4')	
-	logits = slim.fully_connected(x, num_classes, scope='fc/fc_1')
+	logits = slim.fully_connected(net, num_classes, scope='fc/fc_1')
 	end_points = ['none']
 	return logits, end_points
 
