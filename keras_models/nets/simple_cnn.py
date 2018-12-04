97268a3e6a2c5fb08c80caf8b54a7a30cbd2a736
z <phxv@mail.ru>
2018-12-05 02:33:41 +0300

    1

--- keras_models/nets/simple_cnn.py
+++ keras_models/nets/simple_cnn.py
@@ -25,8 +25,8 @@ def cnn_2(inputs, num_classes=1000, is_training=True):
 	x = slim.conv2d(x, 32, [5,5], scope='conv4')
 	x = slim.max_pool2d(x, [2,2], scope='pool4')  # 8 x 8 x 32
 	x = slim.flatten(x, scope='flatten3')
-	x = slim.fully_connected(x, 200, scope='fc_hid')	
-	logits = slim.fully_connected(x, num_classes, scope='fc_last')
+	x = slim.fully_connected(x, 200, activation_fn=tf.nn.sigmoid, scope='fc_hid')	
+	logits = slim.fully_connected(x, num_classes, activation_fn=None, scope='fc_last')
 	end_points = ['none']
 	return logits, end_points
 
