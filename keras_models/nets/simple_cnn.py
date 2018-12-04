2234f5b41f052d4cd077d9c58e5f0b730ac59dc8
z <phxv@mail.ru>
2018-12-05 02:23:10 +0300

    1

--- keras_models/nets/simple_cnn.py
+++ keras_models/nets/simple_cnn.py
@@ -7,7 +7,7 @@ def cnn_3(inputs, num_classes=1000, is_training=True):
 	x = tf.reshape(inputs, [-1, IMAGE_SIZE[0], IMAGE_SIZE[1], 3])  # 128 x 128 x 3
 	x = slim.conv2d(x, 16, [5,5], scope='conv1')   
 	x = slim.flatten(x, scope='flatten3')
-	#x = slim.fully_connected(x, 1000, scope='fc_hid')	
+	x = slim.fully_connected(x, 1000, scope='fc_hid')	
 	logits = slim.fully_connected(x, num_classes, scope='fc_last')
 	end_points = ['none']
 	return logits, end_points
