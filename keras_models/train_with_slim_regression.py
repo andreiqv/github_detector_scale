db00d1d90f9f1045af89518aaf5367fceeabacbe
z <phxv@mail.ru>
2018-12-06 00:24:46 +0300

    1

--- keras_models/train_with_slim_regression.py
+++ keras_models/train_with_slim_regression.py
@@ -258,7 +258,7 @@ if __name__ == '__main__':
 
 							if True:
 								for j in range(len(labels)):
-									if  np.argmax(labels[j]) !=  np.argmax(train_outputs[j]):
+									if  labels[j] !=  train_outputs[j]:
 										print('train:', i, j, labels[j], train_outputs[j])
 
 							timer('epoch={} i={}: train loss={:.4f}, acc={:.4f}'.\
