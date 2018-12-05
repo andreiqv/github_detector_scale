6a814e4c9fb9fdc2d22481e593b87d09389f591d
z <phxv@mail.ru>
2018-12-05 12:21:50 +0300

    1

---
+++ keras_models/models_regression.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+from tensorflow import keras
+layers = keras.layers
+from tensorflow.contrib.slim.nets import inception
+from tensorflow.contrib.slim.nets import resnet_v1, resnet_v2
+from tensorflow.contrib.slim.nets import vgg
+from tensorflow.contrib.slim.nets import alexnet	
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2  #224x224.
+
+
+def model_ResNet50(inputs):
+
+	base_model = ResNet50(weights='imagenet', include_top=False, pooling='avg', 
+		input_tensor=inputs)
+	x = base_model.output
+	x = layers.Dense(2, activation='softmax')(x)
+	model = keras.Model(inputs=inputs, outputs=x, name='keras_ResNet50')	
+	return model
+
+def model_InceptionV3(inputs):
+
+	base_model = InceptionV3(weights='imagenet', include_top=False, pooling='avg', 
+		input_tensor=inputs)
+	x = base_model.output
+	x = layers.Dense(5, activation='sigmoid')(x)
+	model = keras.Model(inputs=inputs, outputs=x, name='keras_InceptionV3')	
+	return model
+	
+def model_MobileNetV2(inputs):
+	""" 
+	num_layers: 157
+	"""
+
+	base_model = MobileNetV2(weights=None, alpha=1.0, depth_multiplier=0.35,
+		include_top=False, pooling='avg', input_tensor=inputs)
+	x = base_model.output
+	x = layers.Dense(5, activation='sigmoid')(x)
+	model = keras.Model(inputs=inputs, outputs=x, name='keras_MobileNetV2')
+	return model	
