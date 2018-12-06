00031e48c9aa4845229948e429a81c7f5df35141
z <phxv@mail.ru>
2018-12-06 17:19:43 +0300

    1

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -54,12 +54,12 @@ def lr_scheduler(epoch, lr):
 inputs = keras.layers.Input(shape=(128, 128, 3))
 #model = models.model1(inputs)  # val_miou: 0.0517 ->  0.0855
 #model = models.model3(inputs)  # val_miou: 0.7457 -> 0.7912
-#model = models.model_first2(inputs) #val_miou: 0.4919 -> 0.5159 
+model = models.model_first2(inputs) #val_miou: 0.4919 -> 0.5159 
 #model = models.model_first(inputs)
 
 import new_keras_models.keras_darknet19 as keras_darknet19
 
-model = keras_darknet19.darknet19(inputs)
+#model = keras_darknet19.darknet19(inputs)
 
 import models2
 #model = models2.model_InceptionV3(inputs)
@@ -80,7 +80,7 @@ print('model.trainable_weights:', len(model.trainable_weights))
 #print('model.trainable_weights:', len(model.trainable_weights))
 
 
-model.compile(optimizer=keras.optimizers.Adam(lr=0.001),
+model.compile(optimizer=keras.optimizers.Adam(lr=0.002),
               #optimizer='adagrad',
               #optimizer='adam',
               loss=bboxes_loss,
