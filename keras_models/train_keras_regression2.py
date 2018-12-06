6a1b2fc2b8ba45f4fa2369c75c01fca767e10b86
z <phxv@mail.ru>
2018-12-06 15:04:05 +0300

    1

--- keras_models/train_keras_regression2.py
+++ keras_models/train_keras_regression2.py
@@ -72,7 +72,7 @@ model.compile(optimizer=keras.optimizers.Adam(lr=0.01),
               #optimizer='adagrad',
               #optimizer='adam',
               loss=bboxes_loss,
-              metrics=[accuracy, miou])
+              metrics=[accuracy])
 
 # model = keras.models.load_model(
 #     "./checkpoints/model2-106-0.991-0.991[0.645].hdf5",
