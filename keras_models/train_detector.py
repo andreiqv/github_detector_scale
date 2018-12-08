081eb3b6c7fd3cf66f0918a493ed9ffcc97f7672
z <phxv@mail.ru>
2018-12-08 22:01:51 +0300

    1

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -60,11 +60,11 @@ inputs = keras.layers.Input(shape=(128, 128, 3))
 
 #model = models.model1(inputs)  # val_miou: 0.0517 -> 0.0855
 #model = models.model2(inputs)  # val_miou: 0.6534 -> 0.7436
-#model = models.model3(inputs)  # val_miou: 0.7457 -> 0.7912
+model = models.model3(inputs)  # val_miou: 0.7457 -> 0.7912
 #model = models.model4(inputs)  # val_miou:  0.7663 -> 0.7925
 #model = models.model_first(inputs)  # val_miou: 0.7519 ->  0.7715
 #model = models.model_first2(inputs) # val_miou: 0.7731  -> 0.8045 
-model = models.model_first_3(inputs)
+#model = models.model_first_3(inputs)
 
 import models2
 #model = models2.model_InceptionV3(inputs)
