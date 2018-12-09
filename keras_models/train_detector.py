54f1ec71c40e753541932a89d74477eeacb22611
z <phxv@mail.ru>
2018-12-09 16:22:15 +0300

    1

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -63,12 +63,12 @@ inputs = keras.layers.Input(shape=(128, 128, 3))
 #model = models.model3(inputs)  # val_miou: 0.7457 -> 0.7912
 #model = models.model4(inputs)  # val_miou:  0.7663 -> 0.7925
 #model = models.model_first(inputs)  # val_miou: 0.7519 ->  0.7715
-#model = models.model_first2(inputs) # val_miou: 0.7731  -> 0.8045 
+model = models.model_first2(inputs) # val_miou: 0.7731  -> 0.8045 
 #model = models.model_first_3(inputs)
 
 import models2
 #model = models2.model_InceptionV3(inputs)
-model = models2.model_ResNet50(inputs)   #  0.7738 -> 0.8062
+#model = models2.model_ResNet50(inputs)   #  0.7738 -> 0.8062
 #model = models2.model_MobileNetV2(inputs)
 
 #import new_keras_models.keras_darknet19 as keras_darknet19
