53c7c9b33dce633dcbd0529604e360716035d006
z <phxv@mail.ru>
2018-12-11 08:11:23 +0300

    1

--- keras_models/models2.py
+++ keras_models/models2.py
@@ -123,24 +123,22 @@ def model_cnn_128_v2(inputs):
 
 
 def model_cnn_128_v3(inputs):
-	""" val_accuracy: 0.9879 - val_miou: 0.6895  | val_miou: 0.7115
-	with batchnorm: val_miou: 0.0750
-	add s=2: 0.7611 | val_miou: 0.7842
+	""" 
 	"""	
 	x = inputs 
-	x = conv(x, 8, 3, s=2)
-	x = conv(x, 8, 3)
+	x = conv(x, 8, 5, s=2)
+	x = conv(x, 8, 5)
 	x = maxpool(x)  # 64
-	x = conv(x, 16, 3, s=2)
+	x = conv(x, 16, 5, s=2)
 	x = conv(x, 16, 3)
 	x = maxpool(x)  # 32
-	x = conv(x, 16, 3, s=2)
+	x = conv(x, 16, 5, s=2)
 	x = conv(x, 16, 3)
 	x = maxpool(x)  # 16
-	x = conv(x, 16, 3, s=2)
+	x = conv(x, 16, 5, s=2)
 	x = conv(x, 16, 3)
 	x = maxpool(x)  # 8
-	x = conv(x, 32, 3, s=2)
+	x = conv(x, 32, 5, s=2)
 	x = conv(x, 32, 3)
 	x = maxpool(x)  # 4 x 4 x 16
 
