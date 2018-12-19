7fe491265f39fe0ac4f613800480005453adfe9b
z <phxv@mail.ru>
2018-12-19 11:16:53 +0300

    1

--- inference/camera_test.py
+++ inference/camera_test.py
@@ -45,7 +45,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# if the `q` key was pressed, break from the loop
 	#if key == ord("q"):
 	#	break
-	time.sleep(1)
+	#time.sleep(1)
 
 
 for i in range(10):
