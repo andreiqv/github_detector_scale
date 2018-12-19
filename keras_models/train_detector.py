c57f53f6aa2ea74cbce89f7d92cfdb94364f72b8
z <phxv@mail.ru>
2018-12-19 18:03:24 +0300

    1

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -87,7 +87,8 @@ import models2
 #model = models3.resnet_keras(inputs)
 
 import resnet_v2
-resnet_v2.build_resnet_18(inputs, 5)
+model = resnet_v2.ResnetBuilder.build_resnet_18(inputs, 5)
+#resnet_v2.build_resnet_18(inputs, 5)
 
 
 # optimizer = tf.train.AdamOptimizer()
