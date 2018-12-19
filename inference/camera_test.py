cf5134bcf89e68977038ab68c509b1355f21fc6a
z <phxv@mail.ru>
2018-12-19 11:13:15 +0300

    1

--- inference/camera_test.py
+++ inference/camera_test.py
@@ -35,7 +35,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# show the frame
 	#cv2.imshow("Frame", image)
 	print(type(image))
-	key = cv2.waitKey(1) & 0xFF
+	#key = cv2.waitKey(1) & 0xFF
  
 	# clear the stream in preparation for the next frame
 	rawCapture.truncate(0)
