cc62776654f08dcad396d79755f61082587e4be7
z <phxv@mail.ru>
2018-12-20 15:12:53 +0300

    1

--- keras_models/aux_regression.py
+++ keras_models/aux_regression.py
@@ -5,7 +5,9 @@ EMPTY_PLATFORM_THRESHOLD = 0.7
 
 def bboxes_loss(labels, logits):
     objectness_loss = tf.contrib.losses.mean_squared_error(logits[:, 4], labels[:, 4])
-    bbox_loss = tf.reduce_mean(tf.squared_difference(logits[:, :4], labels[:, :4]), axis=1)
+    #bbox_loss = tf.reduce_mean(tf.squared_difference(logits[:, :4], labels[:, :4]), axis=1)
+    zeros = tf.zeros([4], tf.float)
+    bbox_loss = tf.reduce_mean(tf.squared_difference(logits[:, :4], zeros), axis=1)
     bbox_loss = tf.reduce_mean(bbox_loss * labels[:, 4])
     return objectness_loss + 1 * bbox_loss
 
