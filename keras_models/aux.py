1929d21cdc8f438af5ab169c5f829ed7c4a2a14f
z <phxv@mail.ru>
2018-12-20 22:02:15 +0300

    1

--- keras_models/aux.py
+++ keras_models/aux.py
@@ -12,9 +12,9 @@ def bboxes_loss(labels, logits):
 
 def bboxes_loss(labels, logits):
     objectness_loss = tf.contrib.losses.mean_squared_error(logits[:, 4], labels[:, 4])
-    bbox_loss = tf.reduce_mean(tf.squared_difference(logits[:, :4], labels[:, :4]), axis=1)
-    #zeros = tf.zeros([4], tf.float32)
-    #bbox_loss = tf.reduce_mean(tf.squared_difference(logits[:, :4], zeros), axis=1)
+    #bbox_loss = tf.reduce_mean(tf.squared_difference(logits[:, :4], labels[:, :4]), axis=1)    
+    zeros = tf.zeros([4], tf.float32)
+    bbox_loss = tf.reduce_mean(tf.squared_difference(logits[:, :4], zeros), axis=1)
     bbox_loss = tf.reduce_mean(bbox_loss * labels[:, 4])
     return objectness_loss + 1 * bbox_loss
 
