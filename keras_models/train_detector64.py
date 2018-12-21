8a60b1131da6e0188da5d95f0386af8ea7df36e3
z <phxv@mail.ru>
2018-12-21 11:58:27 +0300

    1

--- keras_models/train_detector64.py
+++ keras_models/train_detector64.py
@@ -112,7 +112,7 @@ print('model.trainable_weights:', len(model.trainable_weights))
 
 
 model.compile(#optimizer=keras.optimizers.Adam(lr=learning_rate),
-              optimizers.SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True),
+              keras.optimizers.SGD(lr=0.005, decay=1e-6, momentum=0.9, nesterov=True),
               #optimizer='adagrad',
               #optimizer='adam',
               loss=bboxes_loss,
