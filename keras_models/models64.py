be423925a0f472982c233d1ac723b5d987bec0ab
z <phxv@mail.ru>
2018-12-22 01:46:10 +0300

    1

--- keras_models/models64.py
+++ keras_models/models64.py
@@ -221,9 +221,9 @@ def model_first_64_v6(inputs):
 	x1 = conv(x, f=8, k=3, s=2, p='VALID')
 	x1 = bn(x1)
 	x2 = conv(x, f=8, k=3, s=1, p='SAME')
+	x2 = maxpool(x2) # 32
 	x2 = bn(x2)
-	x = layers.concatenate([x1, x2])
-	x = maxpool(x) # 32
+	x = layers.concatenate([x1, x2])	
 	x = bn(x)
 
 	x = conv(x, f=16, k=3, s=2, p='VALID')
