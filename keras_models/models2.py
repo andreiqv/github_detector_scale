1ab57706ccd3816a86ab023f40d0115749434e76
z <phxv@mail.ru>
2018-12-21 09:49:54 +0300

    1

--- keras_models/models2.py
+++ keras_models/models2.py
@@ -200,7 +200,7 @@ def model_first_64(inputs):
 	x = conv(x, f=32, k=3, s=1, p='VALID')
 	x = maxpool(x) # 4	
 	x = bn(x)
-	print(x.get_shape())
+	print('x shape:', x.get_shape())
 
 	x = layers.Flatten()(x)
 	x = layers.Dropout(0.5)(x)
