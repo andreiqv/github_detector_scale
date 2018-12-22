6921891eae1b869499200b3b47a5006905094aa3
z <phxv@mail.ru>
2018-12-22 10:19:17 +0300

    1

--- keras_models/export_model_to_pb.py
+++ keras_models/export_model_to_pb.py
@@ -12,7 +12,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 
-model_name = 'model_first2-309-0.994-0.994[0.746]'
+model_name = 'model_test_64_v5-868-0.994-0.994[0.694]'
 #model_name = 'model_first_3-60-1.000-1.000[0.803]'
 model_name = model_name.rstrip('.hdf5')
 
