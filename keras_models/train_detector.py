14d41908279104941c343d6d4fb002d4c5b17908
z <phxv@mail.ru>
2018-12-23 23:05:05 +0300

    1

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -90,9 +90,9 @@ import models2
 #model = models2.model_first_3_3(inputs)  # val_miou: 0.7970
 #model = models2.model_cnn_128_v3(inputs)
 
-#model = models2.model_InceptionV3(inputs)
+model = models2.model_InceptionV3(inputs)
 #model = models2.model_ResNet50(inputs)   #  0.7738 -> 0.8062
-model = models2.model_MobileNetV2(inputs)
+#model = models2.model_MobileNetV2(inputs) # val_miou: 0.8022
 
 #import new_keras_models.keras_darknet19 as keras_darknet19
 #model = keras_darknet19.darknet19(inputs) # val_miou: 0.6106
