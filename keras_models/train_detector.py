880964e9a69ae4e3386eefd8b7205439fccf37f3
z <phxv@mail.ru>
2018-12-27 02:01:50 +0300

    1

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -82,10 +82,10 @@ import models2
 #model = models2.model_cnn_128(inputs)
 #model = models2.model_cnn_128_v2(inputs)
 
-#model = models2.model_InceptionV3(inputs)  # val_miou: 0.8241
-#model = models2.model_ResNet50(inputs)   #  0.7738 -> 0.8062
-model = models2.model_MobileNetV2(inputs, depth=0.35) # val_miou: 0.8022
-#model = models2.model_MobileNetV2(inputs, depth=1) # 
+model = models2.model_InceptionV3(inputs)  # val_miou: 0.8241
+#model = models2.model_ResNet50(inputs)    #  val_miou: 0.8524
+#model = models2.model_MobileNetV2(inputs, depth=0.35) # val_miou: 0.8022
+#model = models2.model_MobileNetV2(inputs, depth=1) # 0.812
 
 #import new_keras_models.keras_darknet19 as keras_darknet19
 #model = keras_darknet19.darknet19(inputs) # val_miou: 0.6106
@@ -100,7 +100,7 @@ import resnet_v2
 #model = resnet_v2.ResnetBuilder.build_resnet_18(  # val_miou: 0.8286
 #               (image_channels, image_shape[0], image_shape[1]), 5)
 
-model_name = 'model_MobileNetV2_0.35'
+model_name = 'model_InceptionV3'
 model_name = 'presence_' + model_name if presence else model_name
 
 
