188304dbbc13ca779db9da8b1b39022342fd0eee
z <phxv@mail.ru>
2018-12-27 19:42:10 +0300

    1

--- keras_models/modelsaver.py
+++ keras_models/modelsaver.py
@@ -36,7 +36,7 @@ def save(model, path):
 
 if __name__ == '__main__':
 
-	model_name = 'presence_model_MobileNetV2-100-1.000-1.000[0.812]'
+	model_name = 'model_3_2-165-1.000-1.000[0.818]'
 	#model_name = 'model_first_3-60-1.000-1.000[0.803]'
 	model_name = model_name.rstrip('.hdf5')
 
