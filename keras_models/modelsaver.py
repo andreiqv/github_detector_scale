6b9a8a7a296e1eb95c6cfbe4f816910b89691755
z <phxv@mail.ru>
2018-12-27 19:41:35 +0300

    1

--- keras_models/modelsaver.py
+++ keras_models/modelsaver.py
@@ -18,16 +18,30 @@ def freeze_graph(graph, session, output, path):
 def save(model, path):
 
 	keras.backend.set_learning_phase(0)
-	print(base_model.summary())
+	print(model.summary())
 	session = keras.backend.get_session()
 
 	print("model inputs:")
-	for node in base_model.inputs:
+	for node in model.inputs:
 		print(node.op.name)
 
 	print("model outputs:")
-	for node in base_model.outputs:
+	for node in model.outputs:
 		print(node.op.name)
 
 	freeze_graph(session.graph, session, \
-		[out.op.name for out in base_model.outputs], path)
+		[out.op.name for out in model.outputs], path)
+
+
+
+if __name__ == '__main__':
+
+	model_name = 'presence_model_MobileNetV2-100-1.000-1.000[0.812]'
+	#model_name = 'model_first_3-60-1.000-1.000[0.803]'
+	model_name = model_name.rstrip('.hdf5')
+
+	base_model = keras.models.load_model(
+		"./checkpoints/{}.hdf5".format(model_name),
+		custom_objects={'miou': miou, 'accuracy': accuracy, 'bboxes_loss': bboxes_loss})
+	
+	modelsaver.save(model, path='../pb/' + model_name + '.pb')
