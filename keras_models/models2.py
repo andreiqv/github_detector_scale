9c384ae1a6115e8cce04b9b9731df65230c507ec
z <phxv@mail.ru>
2018-12-27 10:23:06 +0300

    1

--- keras_models/models2.py
+++ keras_models/models2.py
@@ -11,6 +11,7 @@ from tensorflow.contrib.slim.nets import alexnet
 from tensorflow.keras.applications.resnet50 import ResNet50
 from tensorflow.keras.applications.inception_v3 import InceptionV3
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2  #224x224.
+from tensorflow.keras.applications.mobilenet import MobileNet  #224x224.
 
 OUTPUT_NAME = 'output'
 
