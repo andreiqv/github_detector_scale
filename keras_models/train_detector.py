5173d2d86dc033b48cd9cd9228d034234c4c25aa
z <phxv@mail.ru>
2018-12-28 02:35:45 +0300

    1

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -106,7 +106,7 @@ import models_resnet
 #model = resnet_v2.ResnetBuilder.build_resnet_18(  # val_miou: 0.8286
 #               (image_channels, image_shape[0], image_shape[1]), 5)
 
-model_name = 'slim_MobileNet_v2_035'
+model_name = 'test'
 model_name = 'presence_' + model_name if presence else model_name
 
 
