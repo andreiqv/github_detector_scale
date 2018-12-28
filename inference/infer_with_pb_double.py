e0f05c9b3cc9096b9559551172853d99efa37e3c
z <phxv@mail.ru>
2018-12-28 11:21:23 +0300

    1

--- inference/infer_with_pb_double.py
+++ inference/infer_with_pb_double.py
@@ -43,7 +43,7 @@ if True:
 	#PB2_PATH = '../pb/model_first_3-102-1.000-1.000[0.808].pb'      # 0.0502 sec.
 	#PB2_PATH = '../pb/presence_resnet18_2-59-1.000-1.000[0.825].pb' # 0.3639 sec. # 'input_1' and 'dense/Sigmoid'
 	#PB2_PATH = '../pb/model_resnet50-97-0.996-0.996[0.833].pb'   # 0.0731 sec.
-	PB2_PATH = '../pb/presence_test.pb' 
+	PB2_PATH = '../pb/presence_test_mobile-01-0.848-0.957[0.383].pb' 
 	#PB2_PATH = '../pb/model_3_2-165-1.000-1.000[0.818].pb'
 
 	INPUT_SIZE_1 = [3, 64, 64]
