4a4076224872d620bfa0d671b614acb53b3d7a2d
z <phxv@mail.ru>
2018-12-28 12:13:04 +0300

    1

--- keras_models/export_model_to_pb.py
+++ keras_models/export_model_to_pb.py
@@ -12,7 +12,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 #model_name = 'presence_model_MobileNetV2-100-1.000-1.000[0.812]'
-model_name = 'presence_model_MobileNetV2_0.35-37-0.999-0.999[0.802]'
+model_name = 'presence_model_MobileNetV2_-152-1.000-1.000[0.846]'
 model_name = model_name.rstrip('.hdf5')
 
 
