70131e80d6fd27b0ef1c7445f923bc0c804ff952
z <phxv@mail.ru>
2019-01-17 00:09:00 +0300

    1

--- keras_models/train_detector.py
+++ keras_models/train_detector.py
@@ -153,15 +153,16 @@ if presence:
     train_path = "../dataset/bg-presence-train-bboxes{}x{}.tfrecords".format(*image_shape)
     test_path = "../dataset/bg-presence-test-bboxes{}x{}.tfrecords".format(*image_shape)
     print('Using presence dataset {}x{}'.format(*image_shape))
-    train_steps = 319 * 128 // batch_size # no pictures with empty scales
-    valid_steps = 3  * 128 // batch_size # no pictures with empty scales
+    train_steps = 586 * 128 // batch_size # no pictures with empty scales
+    valid_steps = 16  * 128 // batch_size # no pictures with empty scales
 
 else:
     train_path = "../dataset/bg-train-bboxes{}x{}.tfrecords".format(*image_shape)
     test_path = "../dataset/bg-test-bboxes{}x{}.tfrecords".format(*image_shape)
     print('Using full dataset {}x{}'.format(*image_shape))
-    train_steps = 499 * 128 // batch_size
-    valid_steps = 3  * 128 // batch_size 
+    train_steps = 787 * 128 // batch_size
+    valid_steps = 20  * 128 // batch_size 
+
 
 print('train dataset path:', train_path)
 print('test  dataset path:', test_path)
